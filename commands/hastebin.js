24d39ea8fd47dc3055d0601e80ee8334f3b539b7
TYPICAL NINJA <seoden2136@gmail.com>
2020-10-27 06:17:50 +0000

    Initial commit

---
+++ commands/hastebin.js
@@ -0,0 +1,24 @@
+const client = require('discord.js');
+const Discord = require('discord.js');
+const Hastebin = require("hastebin-save");
+
+module.exports = {
+	name: 'hastebin',
+	description: '',
+  args: true,
+ usage: '<code>',
+	cooldown: 5,
+	async execute(message,args) {
+const text = args.join(" ")
+
+if(text.length < 6) return message.channel.send("the code to post to hastebin must be over 6 chrs long")
+
+if(text.length > 1000) return message.channel.send("the code to post to hastebin must be under 1000 chrs ")
+
+Hastebin.upload(text, link => {
+  message.delete()
+    message.channel.send("https://hastebin.com/" + link);
+});
+
+   },
+};
---
+++ index.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const  prefix = '!'
+
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+const cooldowns = new Discord.Collection();
+
+client.once('ready', async () => {
+
+
+  client.user.setActivity(`with !play | ${client.guilds.cache.size} servers! `); 
+	console.log('Ready!');
+});
+client.on("disconnected", function () {
+    console.log("Disconnected from Discord");
+    process.exit(1);
+});
+client.on('message', message => {
+if (!message.content.toLowerCase().startsWith(prefix) || message.author.bot) return;
+
+  
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command = client.commands.get(commandName)
+    || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+  if (!command) return;
+
+
+
+
+  if (command.guildOnly && message.channel.type !== 'text') {
+    return message.reply('I can\'t execute that command inside DMs!');
+  }
+
+if(message.channel.guild && !message.guild.me.hasPermission(["VIEW_CHANNEL"])) return message.author.send("I do not have permision to send messages in your channel.ask a admin to give me permision > require > VIEW_CHANNEL")
+
+if(message.channel.guild && !message.guild.me.hasPermission(["SEND_MESSAGES"])) return message.author.send("I do not have permision to send messages in your channel.ask a admin to give me permision > require > SEND_MESSAGES")
+
+
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    }
+
+    return message.channel.send(reply);
+  }
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.channel.send(`ERROR >>: ${error}`);
+  }
+});
+
+client.login(process.env.TOKEN);
